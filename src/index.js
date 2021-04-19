4e993bc1c551f572d711a9b592a3211ea5399118
OrlandoMendoza17 <ommv.17@gmail.com>
2021-04-19 14:32:37 -0200

    Se terminó de realizar la lógica de darkmode en React.js

--- src/index.js
+++ src/index.js
@@ -2,25 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './scss/App.scss';
+import {getDarkMode} from './utils/saveDarkmodeConfig';
 // import { Provider } from 'react-redux';
 // import store from '@store'
 
-ReactDOM.render(<App />, document.getElementById('container'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
-const darkmode = document.getElementById('darkmode')
+// const darkmode = document.getElementById('darkmode')
 
-if(matchMedia('(prefers-color-scheme: dark)').matches){
-  darkmode.setAttribute('checked', true)
-}else{
-  // darkmode.setAttribute('checked', true)
-}
+// if(matchMedia('(prefers-color-scheme: dark)').matches){
+//   darkmode.setAttribute('checked', true)
+// }else{
+//   // darkmode.setAttribute('checked', true)
+// }
 
-darkmode.addEventListener('change', ()=>{
-  if(darkmode.checked){
-    document.body.classList.add('darkmode')    
-    document.body.classList.remove('lightmode')    
-  }else{
-    document.body.classList.add('lightmode')    
-    document.body.classList.remove('darkmode')    
-  }
-})
+// darkmode.addEventListener('change', ()=>{
+//   if(darkmode.checked){
+//     document.body.classList.add('darkmode')    
+//     document.body.classList.remove('lightmode')    
+//   }else{
+//     document.body.classList.add('lightmode')    
+//     document.body.classList.remove('darkmode')    
+//   }
+// })
